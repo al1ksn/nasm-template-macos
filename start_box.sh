36e8aa6b680f97902eb7c4dca4d8f72f7b269b47
Alexander Kalugin <103287576+al1ksn@users.noreply.github.com>
2025-04-20 19:02:56 +0500

    Fixed start_box.sh

--- start_box.sh
+++ start_box.sh
@@ -5,6 +5,5 @@ PROJECT_PATH="$(cd "$(dirname "$0")" && pwd)"
 docker run -d \
   --name nasmbox \
   --mount type=bind,source="${PROJECT_PATH}/src",target=/src \
-  --rm \
   --platform linux/amd64 \
   nasmbox
